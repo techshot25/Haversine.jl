296d74e1268406f51b1e8efb02390bd1c41ad7dc
Ali Shannon <alishanoon@gmail.com>
2021-02-05 20:46:43 -0500

    Fix imports

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using Haversine
 using Test
 
 @testset "Haversine.jl" begin
-    @test HaversineDistance([1, 2], [3, 4]) == 314283.25507368386
+    @test Haversine.HaversineDistance([1, 2], [3, 4]) == 314283.25507368386
 end
